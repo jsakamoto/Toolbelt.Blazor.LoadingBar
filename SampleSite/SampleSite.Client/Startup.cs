6661a67778596b0978bdfc0fc3760d2fe2e366fb
jsakamoto <junichi.jp@gmail.com>
2019-02-08 22:56:30 +0900

    Upgrade Blazor to v.0.8.0

--- SampleSite/SampleSite.Client/Startup.cs
+++ SampleSite/SampleSite.Client/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
@@ -11,7 +11,7 @@ namespace SampleSite.Client
             services.AddLoadingBar();
         }
 
-        public void Configure(IBlazorApplicationBuilder app)
+        public void Configure(IComponentsApplicationBuilder app)
         {
             app.UseLoadingBar();
 
